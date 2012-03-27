2e98fcf136c57c3802c41525b035f91a7448f4c3
Akshet Pandey <argetlam.akshet@gmail.com>
2012-03-27 23:58:23 +0530

    Modified CSServerFinder.py to use unicast instead of directed broadcast, switches don't allow directed broadcast by default

--- workers/CSServerFinder.py
+++ workers/CSServerFinder.py
@@ -12,11 +12,12 @@ class CSServerFinder(DatagramProtocol):
             self.dstPort = 27015
             self.magicList = [0xff, 0xff, 0xff, 0xff, 0x54, 0x53, 0x6f, 0x75, 0x72, 0x63, 0x65, 0x20, 0x45, 0x6e, 0x67, 0x69, 0x6e, 0x65, 0x20, 0x51, 0x75, 0x65, 0x72, 0x79, 0x00]
             self.magicString = struct.pack("!" + "B"*len(self.magicList), *self.magicList)
-            self.ipRanges = ["10.1.33.255", "10.1.34.255", "10.1.35.255", "10.1.36.255", "10.1.39.255", "10.1.40.255",
-                             "10.1.65.255", "10.1.66.255", "10.1.98.255", "10.2.12.255", "10.2.16.255", "10.2.20.255",
-                             "10.2.24.255", "10.2.28.255", "10.2.32.255", "10.2.36.255", "10.2.4.255" , "10.2.40.244",
-                             "10.2.44.255", "10.2.8.255" , "10.3.3.255" , "10.3.4.255" , "10.4.3.255" , "10.5.2.255" ,
-                             "172.17.16.255"]
+#            self.ipRanges = ["10.1.33.255", "10.1.34.255", "10.1.35.255", "10.1.36.255", "10.1.39.255", "10.1.40.255",
+#                             "10.1.65.255", "10.1.66.255", "10.1.98.255", "10.2.12.255", "10.2.16.255", "10.2.20.255",
+#                             "10.2.24.255", "10.2.28.255", "10.2.32.255", "10.2.36.255", "10.2.4.255" , "10.2.40.244",
+#                             "10.2.44.255", "10.2.8.255" , "10.3.3.255" , "10.3.4.255" , "10.4.3.255" , "10.5.2.255" ,
+#                             "172.17.16.255"]
+            self.ipRanges = ["10.1.34.%s", "10.1.35.%s", "10.1.36.%s", "10.1.39.%s", "10.1.40.%s", "10.1.65.%s", "10.1.66.%s"]
             self.transport.getHandle().setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
             self.lastSendTime = None
             self.pingerTask = LoopingCall(self.serverPinger)
@@ -48,7 +49,8 @@ class CSServerFinder(DatagramProtocol):
             self.dbConnection.commit()
             self.lastSendTime = time.time()
             for self.ipAddr in self.ipRanges:
-                self.transport.write(self.magicString, (self.ipAddr, self.dstPort))
+                for i in xrange(2, 254):
+                    self.transport.write(self.magicString, (self.ipAddr % i, self.dstPort))
         except Exception as e:
                 print(e)
 
@@ -64,13 +66,13 @@ class CSServerFinder(DatagramProtocol):
             serverPlayer = ord(serverResponseList[5][0])
             serverPlayerMax = ord(serverResponseList[5][1])
             
-            print ("Server Found" +
-                   "\n\tServer Name : " + serverName +
-                   "\n\tServer IP : " + serverIP + ":" + serverPort +
-                   "\n\tMap Name : " + serverMapName +
-                   "\n\tType : " + serverType +
-                   "\n\tPlayers : " + str(serverPlayer) + "/" + str(serverPlayerMax) +
-                   "\n\tLatency : " + str(serverLatency) + "\n")
+#            print ("Server Found" +
+#                   "\n\tServer Name : " + serverName +
+#                   "\n\tServer IP : " + serverIP + ":" + serverPort +
+#                   "\n\tMap Name : " + serverMapName +
+#                   "\n\tType : " + serverType +
+#                   "\n\tPlayers : " + str(serverPlayer) + "/" + str(serverPlayerMax) +
+#                   "\n\tLatency : " + str(serverLatency) + "\n")
 
             self.dbCursor.execute("insert into cs values (?,?,?,?,?,?,?,?,?)", (serverIP, serverPort, serverName, serverMapName, serverType, serverGameName, serverPlayer, serverPlayerMax, serverLatency))
             self.dbConnection.commit()
